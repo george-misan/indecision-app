2494c0d66c399e937fc57cf2504fad568a0f3827
asentaja <Asentaja@BC-36ZV7S2.bc.local>
2020-04-01 23:13:44 +0300

    picking an option

--- src/app.js
+++ src/app.js
@@ -25,6 +25,14 @@ const removeAll = () => {
   render();
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+
+  console.log(randomNum);
+};
+
 var appRoot = document.getElementById("app");
 
 const numbers = ["100", "203", "111"];
@@ -35,7 +43,9 @@ const render = () => {
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-      <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
+        What should I do?
+      </button>
       <button onClick={removeAll}>Remove all</button>
       {numbers.map(num => {
         return <p key={num}>Number: {num}</p>;
